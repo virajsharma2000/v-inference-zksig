f1d6cc55b646b18b8951771b7c64c4315afa9236
viraj sharma <virajsharma1501@gmail.com>
2025-07-30 00:25:00 +0530

    Update zksk-demo.py

--- zksk-demo.py
+++ zksk-demo.py
@@ -1,3 +1,12 @@
+# This demo uses zksk (a Python library for Sigma protocols). It showcases:
+
+# Alice (the prover) knows a secret x.
+
+# She convinces Bob (the verifier) she knows x without revealing it.
+
+# Similar to “I know the password that opens the cave door.”
+
+
 from zksk import Secret, DLRep, utils
 
 # Shared group generator (elliptic curve group)
